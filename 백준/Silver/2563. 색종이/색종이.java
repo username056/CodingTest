48f4e9d3fabba8a9687847490d4447b087ee4ba9
username056 <84067473+username056@users.noreply.github.com>
2026-01-24 14:23:46 +0900

    [Silver V] Title: 색종이, Time: 184 ms, Memory: 17916 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        int[][] paper = new int[100][100];
+        
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (paper[j][k] == 0) {
+                        paper[j][k] = 1;
+                        cnt++;
+                    }
+                }
+            }
+        }
+        
+        System.out.println(cnt);
+        sc.close();
+    }
+}
