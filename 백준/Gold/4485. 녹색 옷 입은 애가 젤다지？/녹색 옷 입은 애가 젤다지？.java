3f17c5a89f7b41c9e759d6a82705332d87ba53f1
username056 <84067473+username056@users.noreply.github.com>
2026-04-22 00:25:43 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 212 ms, Memory: 21260 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -6,82 +6,60 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static class Node implements Comparable<Node> {
-		int x, y, value;
-
-		Node(int y, int x, int value) {
-			this.x = x;
-			this.y = y;
-			this.value = value;
-		}
-
-		@Override
-		public int compareTo(Main.Node o) {
-			return value - o.value;
-		}
-	}
-
-	static int N;
-	static int arr[][], dist[][];
-	static boolean check[][];
-	static int dx[] = { 0, 0, -1, 1 }, dy[] = { 1, -1, 0, 0 };
+	static int N, arr[][];
+	static int[] dy = { -1, 0, 1, 0 }, dx = { 0, -1, 0, 1 };
 
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		int caseNum = 1;
 		N = Integer.parseInt(bf.readLine());
-		int curNum = 0;
-		// 시작은 (0,0) 도착은(N-1,N-1)
 		while (N != 0) {
-			curNum++;
-			check = new boolean[N][N];
 			arr = new int[N][N];
-			dist = new int[N][N];
+
 			for (int i = 0; i < N; i++) {
-				Arrays.fill(dist[i], Integer.MAX_VALUE);
 				StringTokenizer st = new StringTokenizer(bf.readLine());
 				for (int j = 0; j < N; j++) {
 					arr[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 
-			Dijkstra();
-
-			System.out.println("Problem " + curNum + ": " + dist[N - 1][N - 1]);
+			System.out.println("Problem " + caseNum + ": " + func());
 
 			N = Integer.parseInt(bf.readLine());
+			caseNum++;
 		}
 
 	}
 
-	public static void Dijkstra() {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.add(new Node(0, 0, arr[0][0]));
-		dist[0][0] = arr[0][0];
+	static int func() {
+		int temp[][] = new int[N][N];
+		for (int i = 0; i < N; i++)
+			Arrays.fill(temp[i], Integer.MAX_VALUE);
+		temp[0][0] = arr[0][0];
 
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[2], b[2]));
+
+		pq.add(new int[] { 0, 0, arr[0][0] });
 		while (!pq.isEmpty()) {
-			Node cur = pq.poll();
-
-			if (cur.value != dist[cur.y][cur.x]) 
-				continue;
-
-			if (cur.x == N - 1 && cur.y == N - 1)
-				return;
-			
-			check[cur.y][cur.x] = true;
+			int cur[] = pq.poll();
+			if (cur[0] == N - 1 && cur[1] == N - 1)
+				return cur[2];
 
 			for (int i = 0; i < 4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
-				if (nx < 0 || ny < 0 || nx > N - 1 || ny > N - 1)
+				int ny = cur[0] + dy[i];
+				int nx = cur[1] + dx[i];
+				if (ny < 0 || nx < 0 || ny > N - 1 || nx > N - 1)
 					continue;
-				if (dist[ny][nx] > dist[cur.y][cur.x] + arr[ny][nx]) {
-					dist[ny][nx] = dist[cur.y][cur.x] + arr[ny][nx];
-
-					pq.add(new Node(ny, nx, dist[ny][nx]));
+				if (temp[ny][nx] > cur[2] + arr[ny][nx]) {
+					temp[ny][nx] = cur[2] + arr[ny][nx];
+					pq.add(new int[] { ny, nx, cur[2] + arr[ny][nx] });
 				}
 			}
-
 		}
 
+		return -1;
 	}
+
 }
